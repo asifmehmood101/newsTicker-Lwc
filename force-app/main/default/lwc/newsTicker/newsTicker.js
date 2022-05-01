5ce5bf197c065772e496428038819d06bd154ec3
asifmehmood101 <amehmood999@gmail.com>
2022-05-01 12:51:41 +0500

    init commit

---
+++ force-app/main/default/lwc/newsTicker/newsTicker.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+//import CloseICon from "@salesforce/resourceUrl/Close"
+
+export default class NewsTicker extends LightningElement {
+    //NewsTickerIcon = `${CloseICon}`;
+    
+
+
+
+}
