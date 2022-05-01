1308794031182372e1ce88f1f60c1bbda99900e9
asifmehmood101 <amehmood999@gmail.com>
2022-05-01 19:59:29 +0500

    add: close button logic and responsiveness

--- force-app/main/default/lwc/newsTicker/newsTicker.js
+++ force-app/main/default/lwc/newsTicker/newsTicker.js
@@ -1,10 +1,28 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 //import CloseICon from "@salesforce/resourceUrl/Close"
 
 export default class NewsTicker extends LightningElement {
     //NewsTickerIcon = `${CloseICon}`;
+
+    @track showNewsTickerBar = true;
+
+
+    handleNewsTickerBar (){
+        this.showNewsTickerBar = false
+    }
+
+    
+    //remove newsTicker bar base on after some time 
     
 
+
+
+    
+
+     
+
+
+    
 
 
 }
