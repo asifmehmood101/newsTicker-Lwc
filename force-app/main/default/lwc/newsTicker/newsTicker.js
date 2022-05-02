43e320a9f93e1be1b03c9944c4ff1a6affe9ebf1
asifmehmood101 <amehmood999@gmail.com>
2022-05-03 02:43:03 +0500

    add: big changes

--- force-app/main/default/lwc/newsTicker/newsTicker.js
+++ force-app/main/default/lwc/newsTicker/newsTicker.js
@@ -1,36 +1,36 @@
-import {
-   api,
-   LightningElement,
-   track
-} from 'lwc';
+import { api, LightningElement, track } from "lwc";
 //import CloseICon from "@salesforce/resourceUrl/Close"
 
 export default class NewsTicker extends LightningElement {
-   //NewsTickerIcon = `${CloseICon}`;
+  //NewsTickerIcon = `${CloseICon}`;
 
-   @api customBackgroundColor;
-   @api newsTickerBarTimeout = 6000;
-   @track showNewsTickerBar = true;
+  @api customBackgroundColor;
+  @api fontColor;
+  @api newsTickerBarTimeout = 6000;
+  @track showNewsTickerBar = true;
 
+  renderedCallback() {
+    // CHANGE NEWS TICKER BACKGROUND COLOR
+    if (this.template.querySelector(".rtl-sticky")) {
+      this.template.querySelector(".rtl-sticky").style.backgroundColor =
+        this.customBackgroundColor;
+    }
 
-   renderedCallback() {
-      
-      if (this.template.querySelector(".rtl-sticky")) {
-         this.template.querySelector(".rtl-sticky").style.backgroundColor = this.customBackgroundColor;
+    // CHANGE TEXT COLOR
+    if (this.template.querySelector(".news-titles")) {
+      this.template.querySelector(".news-titles").style.color = this.fontColor;
+    }
+
+    //REMOVE NEWS TICKER BAR AFTER TIMEOUT
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    setTimeout(() => {
+      if (this.showNewsTickerBar) {
+        this.showNewsTickerBar = false;
       }
-      
-      //remove newsTicker bar after some timeout  
-      setTimeout(() => {
-         if (this.showNewsTickerBar) {
-            this.showNewsTickerBar = false;
-         }
-      }, this.newsTickerBarTimeout);
-   }
+    }, this.newsTickerBarTimeout);
+  }
 
-   handleNewsTickerBar() {
-       this.showNewsTickerBar = false
-   }
-
-   
-
+  handleNewsTickerBar() {
+    this.showNewsTickerBar = false;
+  }
 }
