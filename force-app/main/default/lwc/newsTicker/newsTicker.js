dec89e6fb564fd5cf0e130b0dcf398a8b7f164ff
asifmehmood101 <amehmood999@gmail.com>
2022-06-14 19:47:12 +0500

    add news ticker query

--- force-app/main/default/lwc/newsTicker/newsTicker.js
+++ force-app/main/default/lwc/newsTicker/newsTicker.js
@@ -1,106 +1,107 @@
-import {api, LightningElement, track} from "lwc";
+import { api, LightningElement, track } from "lwc";
 
-import listSObjects from '@salesforce/apex/SObjectCRUDController.listSObjects';
+import listSObjects from "@salesforce/apex/SObjectCRUDController.listSObjects";
 
 export default class NewsTicker extends LightningElement {
-    @api customBackgroundColor;
-    @api fontColor;
-    @api animationDuration;
-    @api animationDelay;
-    @api newsTickerBarTimeout = 6000;
-    @track showNewsTickerBar = true;
-    @track setWidth;
+  @api customBackgroundColor;
+  @api fontColor;
+  @api animationDuration;
+  @api animationDelay;
+  @api newsTickerBarTimeout = 6000;
+  @track showNewsTickerBar = true;
+  @track allmessages;
 
-    // temp mock data
-    newsItems = [
-        {
-            id: 0,
-            message:
-                "0 Your browser is outdated. Your Salesforce experience may be degraded.Your browser is outdated. ",
-            link: "www.google.com"
-        },
+  // temp mock data
+  newsItems = [
+    {
+      id: 0,
+      message:
+        "0 Your browser is outdated. Your Salesforce experience may be degraded.Your browser is outdated. ",
+      link: "www.google.com"
+    },
 
-        {
-            id: 1,
-            message:
-                "1 Your browser is outdated. Your Salesforce experience may be degraded.Your browser is outdated. ",
-            link: "www.google.com"
-        },
-        {
-            id: 2,
-            message:
-                "2 Your browser is outdated. Your Salesforce experience may be degraded.Your browser is outdated.",
-            link: "www.google.com"
-        },
-        {
-            id: 3,
-            message:
-                "3 Your browser is outdated. Your Salesforce experience may be degraded.Your browser is outdated.",
-            link: "www.google.com"
-        },
-        {
-            id: 4,
-            message:
-                "4 Your browser is outdated. Your Salesforce experience may be degraded.Your browser is outdated.",
-            link: "www.google.com"
-        },
-        {
-            id: 5,
-            message:
-                "5 Your browser is outdated. Your Salesforce experience may be degraded.Your browser is outdated.Your Salesforce experience may be degraded.Your browser is outdated.",
-            link: "www.google.com"
-        }
-    ];
+    {
+      id: 1,
+      message:
+        "1 Your browser is outdated. Your Salesforce experience may be degraded.Your browser is outdated. ",
+      link: "www.google.com"
+    },
+    {
+      id: 2,
+      message:
+        "2 Your browser is outdated. Your Salesforce experience may be degraded.Your browser is outdated.",
+      link: "www.google.com"
+    },
+    {
+      id: 3,
+      message:
+        "3 Your browser is outdated. Your Salesforce experience may be degraded.Your browser is outdated.",
+      link: "www.google.com"
+    },
+    {
+      id: 4,
+      message:
+        "4 Your browser is outdated. Your Salesforce experience may be degraded.Your browser is outdated.",
+      link: "www.google.com"
+    },
+    {
+      id: 5,
+      message:
+        "5 Your browser is outdated. Your Salesforce experience may be degraded.Your browser is outdated.Your Salesforce experience may be degraded.Your browser is outdated.",
+      link: "www.google.com"
+    }
+  ];
 
-    renderedCallback() {
-        if (this.template.querySelector(".rtl-sticky")) {
-            // background props for experience builder
-            this.template.querySelector(".rtl-sticky").style.backgroundColor =
-                this.customBackgroundColor;
+  renderedCallback() {
+    if (this.template.querySelector(".rtl-sticky")) {
+      // background props for experience builder
+      this.template.querySelector(".rtl-sticky").style.backgroundColor =
+        this.customBackgroundColor;
 
-            // text props for experience builder
-            this.template.querySelector(".news-titles").style.color = this.fontColor;
+      // text props for experience builder
+      this.template.querySelector(".news-titles").style.color = this.fontColor;
 
-            //Animation props for experience builder
-            this.template.querySelector(
-                ".news-titles"
-            ).style.animationDuration = `${this.animationDuration}s`;
+      //Animation props for experience builder
+      this.template.querySelector(
+        ".news-titles"
+      ).style.animationDuration = `${this.animationDuration}s`;
 
-            this.template.querySelector(
-                ".news-titles"
-            ).style.animationDelay = `${this.animationDelay}s`;
-        }
-
-        //REMOVE NEWS TICKER BAR AFTER TIMEOUT
-        // eslint-disable-next-line @lwc/lwc/no-async-operation
-        setTimeout(() => {
-            if (this.showNewsTickerBar) {
-                this.showNewsTickerBar = false;
-            }
-        }, this.newsTickerBarTimeout);
+      this.template.querySelector(
+        ".news-titles"
+      ).style.animationDelay = `${this.animationDelay}s`;
     }
 
-    async connectedCallback() {
-        await this.loadNewsMessage();
-    }
-
-    async loadNewsMessage() {
-
-        const newsMessages = await listSObjects({
-            objectName: 'News_Message__c',
-            fields: [
-                'Message__c',
-                'StartDate__c',
-                'EndDate__c',
-                'RecommendationAudiences__c'
-            ],
-            filters: `ORDER BY StartDate__c ASC LIMIT 5`
-        });
-
-        console.log('news message', newsMessages);
-    }
-
-    handleNewsTickerBar() {
+    //REMOVE NEWS TICKER BAR AFTER TIMEOUT
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    setTimeout(() => {
+      if (this.showNewsTickerBar) {
         this.showNewsTickerBar = false;
-    }
+      }
+    }, this.newsTickerBarTimeout);
+  }
+
+  async connectedCallback() {
+    await this.loadNewsMessage();
+  }
+
+  async loadNewsMessage() {
+    const newsMessages = await listSObjects({
+      objectName: "News_Message__c",
+      fields: [
+        "Message__c",
+        "StartDate__c",
+        "EndDate__c",
+        "RecommendationAudiences__c"
+      ],
+      filters: `ORDER BY StartDate__c ASC LIMIT 5`
+    });
+
+    this.allmessages = newsMessages;
+
+    console.log("news message", newsMessages);
+  }
+
+  handleNewsTickerBar() {
+    this.showNewsTickerBar = false;
+  }
 }
